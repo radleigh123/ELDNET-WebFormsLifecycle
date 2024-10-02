f890615d3e5a04a4c2873b306c9bcb6ed41eda2f
Radleigh123 <103168649+radleigh123@users.noreply.github.com>
2024-10-02 18:32:01 +0800

    Initial commit

---
+++ WebFormsLifecycle/Main.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Main.aspx.cs" Inherits="WebFormsLifecycle.Main" %>
+
+<!DOCTYPE html>
+
+<html lang="en">
+<head runat="server">
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Main page</title>
+    <link href="Content/bootstrap.min.css" rel="stylesheet" />
+</head>
+<body style="background-color: rgb(128, 128, 128);">
+    <form id="form1" runat="server">
+        <br />
+        <div class="container px-4">
+            <div class="row gx-5" style="height: 600px;">
+                <div class="col p-0">
+                    <div class="p-4 border border-4 bg-light rounded-2 h-75">
+                        <asp:Image CssClass="img-fluid rounded mx-auto d-block" Style="width: 400px; height:400px;" Visible="false" ID="cat" runat="server" ImageUrl="~/Content/cat/cat1.png" />
+                           <%-- position-relative top-50 start-50 translate-middle rounded-3--%>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="p-3 border bg-light rounded-2 h-75">
+                        <p class="position-relative top-50 start-50 translate-middle text-center">
+                            <asp:Label ID="ClickLabel" runat="server" Text="Click to continue"></asp:Label>
+                            <br />
+                            <asp:Button ID="ClickMe_BTN" runat="server" Text="&#128176;" CssClass="btn btn-danger w-50 mt-3" OnClick="ClickMe_BTN_Click1"></asp:Button>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </form>
+</body>
+</html>
---
+++ WebFormsLifecycle/Main.aspx.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebFormsLifecycle
+{
+    public partial class Main : System.Web.UI.Page
+    {
+        /*protected void Page_PreInit(object sender, EventArgs e)
+        {
+            string preInit = @"
+                <div class='d-grid gap-2 col-6 mx-auto mt-3'>
+                    <div class='shadow-sm p-1 m-0 bg-info rounded bg-gradient'>
+                        <p class='text-success-50 p-0 m-0'>&#128221;  Page_PreInit event executed.</p>
+                    </div>
+                </div>";
+            Response.Write(preInit);
+            Response.End();
+        }
+
+        protected void Page_Init(object sender, EventArgs e)
+        {
+            string pageInit = @"
+                <div class='d-grid gap-2 col-6 mx-auto mt-3'>
+                    <div class='shadow-sm p-1 m-0 bg-info rounded bg-gradient'>
+                        <p class='text-success-50 p-0 m-0'>&#128190;  Page_Init event executed.</p>
+                    </div>
+                </div>";
+            Response.Write(pageInit);
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string pageLoad = @"
+                <div class='d-grid gap-2 col-6 mx-auto mt-3'>
+                    <div class='shadow-sm p-1 m-0 bg-secondary rounded bg-gradient'>
+                        <p class='text-success-50 p-0 m-0'>&#128204;  Page_Load event executed for the first time.</p>
+                    </div>
+                </div>";
+            if (!IsPostBack)
+            {
+                Response.Write(pageLoad);
+            }
+            else
+            {
+                pageLoad = @"
+                <div class='d-grid gap-2 col-6 mx-auto mt-3'>
+                    <div class='shadow-sm p-1 m-0 bg-info rounded bg-gradient'>
+                        <p class='text-success-50 p-0 m-0'>&#128176;  Page_Load event executed on postback.</p>
+                    </div>
+                </div>";
+
+                Response.Write(pageLoad);
+            }
+        }
+
+        protected void Page_Unload(object sender, EventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("Page_Unload event executed.");
+            //Response.Write("Page_Unload event executed.<br />");
+        }
+
+        protected override void Render(HtmlTextWriter writer)
+        {
+            string render = @"
+                <div class='d-grid gap-2 col-6 mx-auto mt-3'>
+                    <div class='shadow-sm p-1 m-0 bg-info rounded bg-gradient'>
+                        <p class='text-success-50 p-0 m-0'>&#128221;  Page_Render event executed.</p>
+                    </div>
+                </div>";
+            Response.Write(render);
+
+            base.Render(writer);
+        }
+
+        protected void ClickMe_BTN_Click1(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+
+            if (ClickLabel.Text == "Cat is shy")
+            {
+                btn.CssClass = "btn btn-success w-50 mt-3";
+                ClickLabel.Text = "Cat is now out";
+                cat.ImageUrl = "~/Content/cat/cat2.png";
+            }
+            else
+            {
+                btn.CssClass = "btn btn-danger w-50 mt-3";
+                ClickLabel.Text = "Cat is shy";
+                cat.ImageUrl = "~/Content/cat/cat1.png";
+            }
+        }*/
+        private string CurrentStage
+        {
+            get { return ViewState["CurrentStage"] as string ?? "Page_PreInit"; }
+            set { ViewState["CurrentStage"] = value; }
+        }
+
+        protected void Page_PreInit(object sender, EventArgs e)
+        {
+            if (CurrentStage == "Page_PreInit")
+            {
+            }
+        }
+
+        protected void Page_Init(object sender, EventArgs e)
+        {
+            if (CurrentStage == "Page_Init")
+            {
+            }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (CurrentStage == "Page_Load")
+            {
+            }
+        }
+
+        protected void Page_PreRender(object sender, EventArgs e)
+        {
+            if (CurrentStage == "Page_PreRender")
+            {
+            }
+        }
+
+        protected void ClickMe_BTN_Click1(object sender, EventArgs e)
+        {
+            switch (CurrentStage)
+            {
+                case "Page_PreInit":
+                    CurrentStage = "Page_Init";
+                    ClickLabel.Text = "Page_PreInit event executed.";
+                    cat.Visible = true;
+                    cat.ImageUrl = "~/Content/images/page1.png";
+                    break;
+                case "Page_Init":
+                    CurrentStage = "Page_Load";
+                    ClickLabel.Text = "Page_Init event executed.";
+                    cat.ImageUrl = "~/Content/images/page2.png";
+                    break;
+                case "Page_Load":
+                    CurrentStage = "Page_PreRender";
+                    ClickLabel.Text = "Page_PreRender event executed.";
+                    cat.ImageUrl = "~/Content/images/page3.png";
+                    break;
+                case "Page_PreRender":
+                    ClickLabel.Text = "Lifecycle completed!";
+                    cat.ImageUrl = "~/Content/images/page4.png";
+                    ClickMe_BTN.Enabled = false; // Disable button
+                    break;
+            }
+        }
+    }
+}
